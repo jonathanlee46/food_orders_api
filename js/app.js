96e7caf8a645b72fbf7b78977962fa1276c980e8
Jonathan Lee <jlee@nisum.com>
2016-03-25 17:01:51 -0700

    change local json to api call

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 // Define the model
 Post = Backbone.Model.extend({
-
+    // urlRoot : 'test.json',
+    urlRoot: 'http://jl46.x10host.com/?json=1',
+      parse: function(response) {
+          console.log(response);
+          return response;
+      }
 });
 
 // Define the collection
@@ -9,21 +14,21 @@ Posts = Backbone.Collection.extend(
         model: Post,
         // Url to request when fetch() is called
         url: 'http://jl46.x10host.com/?json=1',
-        parse: function(response) {
-            return response;
-        },
+        // parse: function(response) {
+        //     return response;
+        // },
         // Overwrite the sync method to pass over the Same Origin Policy
-        sync: function(method, model, options) {
-            var that = this;
-                var params = _.extend({
-                    type: 'GET',
-                    dataType: 'jsonp',
-                    url: that.url,
-                    processData: false
-                }, options);
+        // sync: function(method, model, options) {
+        //     var that = this;
+        //         var params = _.extend({
+        //             type: 'GET',
+        //             dataType: 'json',
+        //             url: that.url,
+        //             processData: false
+        //         }, options);
 
-            return $.ajax(params);
-        }
+        //     return $.ajax(params);
+        // }
     });
 
 // Define the View
