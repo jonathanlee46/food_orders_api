c34644f563e0c5f18bd4b2dd02e84c1a22fc545d
Jonathan Lee <jlee@nisum.com>
2016-03-30 10:51:22 -0700

    rewrite sync function

--- js/app.js
+++ js/app.js
@@ -13,22 +13,23 @@ Posts = Backbone.Collection.extend(
     {
         model: Post,
         // Url to request when fetch() is called
-        url: 'http://jl46.x10host.com/?json=1',
-        // parse: function(response) {
-        //     return response;
-        // },
-        // Overwrite the sync method to pass over the Same Origin Policy
-        // sync: function(method, model, options) {
-        //     var that = this;
-        //         var params = _.extend({
-        //             type: 'GET',
-        //             dataType: 'json',
-        //             url: that.url,
-        //             processData: false
-        //         }, options);
+        // url: 'http://jl46.x10host.com/?json=1',
+        url: 'http://enigmatic.x10host.com/?json=1',
+        parse: function(response) {
+            return response;
+        },
+        Overwrite the sync method to pass over the Same Origin Policy
+        sync: function(method, model, options) {
+            var that = this;
+                var params = _.extend({
+                    type: 'GET',
+                    dataType: 'json',
+                    url: that.url,
+                    processData: false
+                }, options);
 
-        //     return $.ajax(params);
-        // }
+            return $.ajax(params);
+        }
     });
 
 // Define the View
