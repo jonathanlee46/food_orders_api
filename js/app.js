3d5f35773a208077575a9ed232fbe6198c10e1ec
Jonathan Lee <jlee@nisum.com>
2016-03-30 12:22:00 -0700

    parsing code

--- js/app.js
+++ js/app.js
@@ -1,51 +1,44 @@
 // Define the model
 Post = Backbone.Model.extend({
-    // urlRoot : 'test.json',
-    urlRoot: 'http://jl46.x10host.com/?json=1',
-      parse: function(response) {
-          console.log(response);
-          return response;
-      }
+    defaults: {
+        title: "here's a title",
+        id: 999
+    }
 });
 
 // Define the collection
 Posts = Backbone.Collection.extend(
-    {
-        model: Post,
-        // Url to request when fetch() is called
-        // url: 'http://jl46.x10host.com/?json=1',
-        url: 'http://enigmatic.x10host.com/?json=1',
-        parse: function(response) {
-            return response;
-        },
-        Overwrite the sync method to pass over the Same Origin Policy
-        sync: function(method, model, options) {
-            var that = this;
-                var params = _.extend({
-                    type: 'GET',
-                    dataType: 'json',
-                    url: that.url,
-                    processData: false
-                }, options);
-
-            return $.ajax(params);
+{
+    model: Post,
+    // Url to request when fetch() is called
+    // url: 'http://jl46.x10host.com/?json=1',
+    url: 'http://enigmatic.x10host.com/?json=1',
+    // Overwrite the sync method to pass over the Same Origin Policy
+    parse: function(response) {
+        var postArray = response["posts"];
+        console.log(postArray);
+        for (var i = 0; i< postArray.length-1; i++){
+            console.log(postArray[i]);
+            this.add(postArray[i]);
         }
-    });
+        return this;
+    }
+});
+
 
 // Define the View
 PostsView = Backbone.View.extend({
     initialize: function() {
-      _.bindAll(this, 'render');
+        _.bindAll(this, 'render');
 
-      this.collection = new Posts;
-
-      var that = this;
-      this.collection.fetch({
-        success: function () {
-            console.log("fetching");
-            that.render();
-        }
-      });
+        this.collection = new Posts;
+        var that = this;
+        this.collection.fetch({
+            success: function () {
+                console.log("fetching");
+                that.render();
+            }
+        });
     },
     // Use an extern template
 
