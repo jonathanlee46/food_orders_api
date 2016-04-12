1eb2e491ac0b3f7fee2b1a5f35f0691a9fc98ad0
Jonathan Lee <jlee@nisum.com>
2016-04-12 12:25:48 -0700

    added collection

--- js/app.js
+++ js/app.js
@@ -1,56 +1,71 @@
-// Define the model
-Post = Backbone.Model.extend({
-    defaults: {
-        title: "here's a title",
-        id: 999
+// // Define the model
+Order = Backbone.Model.extend({
+    defaults:{
+        name: 'Jack',
+        food: 'Pizza'
+    },
+
+    url: 'http://jl46.x10host.com/?json=1',
+
+    parse: function(response) {
+        var postArray = response["posts"];
+
+        for (var i = 0; i< postArray.length-1; i++){
+
+            //get wp post contents
+            var newName = postArray[i]["title"];
+            console.log(newName);
+            var newFood = postArray[i]["content"];
+            console.log(newOrder);
+
+            //create model instance and store to collection
+            var newOrder = new Order({name: newName, food: newFood});
+            console.log(newOrder.get("name"));
+
+            bill.add(newOrder);  //hardcoded collection to store
+        }
     }
 });
 
 // Define the collection
-Posts = Backbone.Collection.extend(
-{
-    model: Post,
-    // Url to request when fetch() is called
-    // url: 'http://jl46.x10host.com/?json=1',
-    url: 'http://enigmatic.x10host.com/?json=1',
-    // Overwrite the sync method to pass over the Same Origin Policy
-    parse: function(response) {
-        var postArray = response["posts"];
-        console.log(postArray);
-        for (var i = 0; i< postArray.length-1; i++){
-            console.log(postArray[i]);
-            this.add(postArray[i]);
-        }
-        return this;
-    }
-});
-
+OrderCollection = Backbone.Collection.extend({
+    model: Order,
+    url: 'http://jl46.x10host.com/?json=1',
+    
+})
 
 // Define the View
 PostsView = Backbone.View.extend({
     initialize: function() {
-        _.bindAll(this, 'render');
 
-        this.collection = new Posts();
+        //fetch using model, which will store to collection
         var that = this;
-        this.collection.fetch({
+        this.model.fetch({
             success: function () {
-                console.log("fetching");
-                that.render();
+                console.log("fetching"); //proof of fetch
+                that.render(); //call render function
             }
         });
     },
-    // Use an extern template
 
     render: function() {
-        // Fill the html with the template and the collection
-        $(this.el).append("<br>hello world");
-        $(this.el).append(this.collection);
-        $(this.el).append("<br>end");
+        //test to see if collection populated
+        console.log("number of orders on the bill is " + bill.length);
+
+        //print contents to html
+        for (var i = 0; i < bill.length; i++){
+            var tempModel = bill.at(i);
+            $("#post").append(tempModel.get("food"));
+        }
     }
 });
 
+//instances
+var bill = new OrderCollection();
+var order = new Order();
 var app = new PostsView({
     // define the el where the view will render
+    model: order,
+    collection: bill,
     el: $('body')
 });
